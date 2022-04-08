51d6d3900d9c9a73ca3cc491c3e86704e164476c
Kurt Portelli <kurt.portelli@gig.com>
2022-04-08 11:19:40 +0200

    feat: get all data not just books

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lotr
 FastAPI based on the epic Lord of the Rings
+
+### Fetching data from source API
+At the moment, the first step is to fetch all data from the source api by making a request to `/fetch_data` endpoint,
+effectively calling the `fetch_data_from_source_api()` function.
+
+In the future, this data will be polled routinely via a cronjob.
--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 source_api_base_path = os.getenv("SOURCE_API_BASE_PATH")
+access_token = os.getenv("ACCESS_TOKEN")
 
 
-@app.get("/all_books")
-def fetch_books():
-    url = os.path.join(source_api_base_path, "book")
-    response = requests.get(url)
+def fetch_items(item):
+    url = os.path.join(source_api_base_path, item)
+    response = requests.get(url, headers={"Authorization": f"Bearer {access_token}"})
     return response.json()
+
+@app.get("/fetch_data")
+def fetch_data_from_source_api():
+    items = ["book", "movie", "character", "quote", "chapter"]
+    return {f"{item}s_data": fetch_items(item) for item in items}
